d5bc6b209077b28e759fc7b8305829421b0c41b5
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-10-27 20:53:42 +0100

    Try new RTFD theme.

--- docs/conf.py
+++ docs/conf.py
@@ -103,6 +103,8 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
+RTD_NEW_THEME = True
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'default'
