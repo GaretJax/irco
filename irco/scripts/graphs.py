b3dab426ac3c6e211ebdcabecc7bf72dac67738b
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2014-02-10 17:18:33 +0100

    Adding a command line argument to filter by year.

--- irco/scripts/graphs.py
+++ irco/scripts/graphs.py
@@ -3,8 +3,9 @@ import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import true, false
 
-from irco import graphs
+from irco import graphs, models
 
 
 def write(fh, graph, format=None):
@@ -16,6 +17,7 @@ def main():
     graph_choices = graphs.get_graphs()
     argparser = argparse.ArgumentParser('irco-graph')
     argparser.add_argument('-v', '--verbose', action='store_true')
+    argparser.add_argument('-y', '--years')
     argparser.add_argument('graph_type', choices=graph_choices)
     argparser.add_argument('database')
     argparser.add_argument('output', default='-', nargs='?')
@@ -26,8 +28,26 @@ def main():
 
     graph_factory = graphs.get_graph(args.graph_type)
 
+    if args.years:
+        criteria = false()
+        for y in args.years.split(','):
+            if '-' in y:
+                start, end = y.split('-')
+                if start:
+                    c = models.Publication.year >= int(start)
+                else:
+                    c = true()
+
+                if end:
+                    c = c & (models.Publication.year <= int(end))
+            else:
+                c = models.Publication.year == int(y)
+            criteria = criteria | c
+    else:
+        criteria = true()
+
     Session = sessionmaker(bind=engine)
-    graph = graph_factory.create(Session())
+    graph = graph_factory.create(Session(), criteria)
 
     if args.output == '-':
         write(sys.stdout, graph)
