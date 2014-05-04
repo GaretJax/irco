fcd9e0fce2c33f1298069776d18dd6ec4cc59cc3
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2014-05-04 18:50:59 +0200

    Add some example raw SQL queries.

---
+++ sql/papers-count.sql
@@ -0,0 +1,8 @@
+﻿select country, count(*) as papers from (
+	select distinct p.id, i.country
+	from publication as p
+	join affiliated_author as a on a.publication_id = p.id
+	join institution as i on i.id = a.institution_id
+) as s
+group by country
+order by papers desc
---
+++ sql/papers-not-from-kuwait.sql
@@ -0,0 +1,9 @@
+﻿select p.id, p.title
+from publication as p
+where p.id not in (
+	select distinct p.id
+	from publication as p
+	join affiliated_author as a on a.publication_id = p.id
+	join institution as i on i.id = a.institution_id
+	where i.country = 'Kuwait'
+)
